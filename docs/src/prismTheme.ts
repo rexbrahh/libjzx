a92f3c7b1ea883b71ac6795bf6e3355e40063a61
Rex Liu <hi@r3x.sh>
2025-12-14 16:33:03 -0500

    docs: richer code syntax colors

--- docs/src/prismTheme.ts
+++ docs/src/prismTheme.ts
@@ -2,7 +2,7 @@ import type {PrismTheme} from 'prism-react-renderer';
 
 const prismTheme: PrismTheme = {
   plain: {
-    color: '#c2c6cc',
+    color: '#c9d1d9',
     backgroundColor: '#0b0b0b',
   },
   styles: [
@@ -14,45 +14,60 @@ const prismTheme: PrismTheme = {
       },
     },
     {
-      types: ['punctuation', 'operator'],
+      types: ['punctuation'],
       style: {
-        color: '#c2c6cc',
+        color: '#c9d1d9',
       },
     },
     {
       types: [
-        'keyword',
-        'builtin',
-        'builtin-type',
-        'directive',
-        'directive-hash',
+        'keyword', // if/else/return/for (C + Zig)
+        'builtin', // Zig builtins like @import(...)
+        'builtin-type', // Zig builtin types like u32
+        'directive', // C preprocessor directive body
+        'directive-hash', // the leading '#'
         'macro',
         'macro-name',
-        'tag',
-        'attr-name',
-        'selector',
       ],
       style: {
-        color: '#85befd',
+        color: '#ff7b72',
         fontWeight: '600',
       },
     },
     {
-      types: ['function', 'class-name', 'property', 'variable', 'parameter'],
+      types: ['operator'],
       style: {
-        color: '#ffffff',
+        color: '#ff7b72',
+      },
+    },
+    {
+      types: ['function'],
+      style: {
+        color: '#d2a8ff',
+      },
+    },
+    {
+      types: ['class-name'],
+      style: {
+        color: '#ffa657',
+      },
+    },
+    {
+      types: ['property', 'variable', 'parameter', 'expression'],
+      style: {
+        color: '#c9d1d9',
       },
     },
     {
       types: ['string', 'char', 'attr-value', 'regex', 'url'],
       style: {
-        color: '#ffffff',
+        color: '#85befd',
       },
     },
     {
       types: ['number', 'boolean', 'constant', 'symbol'],
       style: {
-        color: '#c2c6cc',
+        color: '#f2cc60',
       },
     },
     {
@@ -70,15 +85,21 @@ const prismTheme: PrismTheme = {
     {
       types: ['deleted'],
       style: {
-        color: '#ffffff',
+        color: '#ff7b72',
         textDecorationLine: 'line-through',
       },
     },
     {
       types: ['inserted'],
       style: {
-        color: '#ffffff',
+        color: '#7ee787',
         textDecorationLine: 'underline',
+      },
+    },
+    {
+      types: ['tag', 'attr-name', 'selector'],
+      style: {
+        color: '#85befd',
       },
     },
   ],
